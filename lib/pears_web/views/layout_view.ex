8cde15f31ca9e2b1552ad398c58c3d1a44ebe159
Adam Zaninovich <adam.zaninovich@gmail.com>
2018-08-11 13:40:18 -0700

    🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐🍐

--- lib/pears_web/views/layout_view.ex
+++ lib/pears_web/views/layout_view.ex
@@ -1,11 +1,13 @@
 defmodule PearsWeb.LayoutView do
   use PearsWeb, :view
 
-  def render_auth_link(conn = %{assigns: %{user: _user}}) do
-    link "Sign Out", to: auth_path(conn, :signout)
+  def render_auth_link(conn), do: render_auth_link(conn, [])
+
+  def render_auth_link(%{assigns: %{user: _user}} = conn, attrs) do
+    link("Sign Out", [to: auth_path(conn, :signout)] ++ attrs)
   end
 
-  def render_auth_link(conn) do
-    link "Sign in with Github", to: auth_path(conn, :request, "github")
+  def render_auth_link(conn, attrs) do
+    link("Sign in with Github", [to: auth_path(conn, :request, "github")] ++ attrs)
   end
 end
